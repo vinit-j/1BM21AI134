1f29decc9dc2e0c5539e4d5d33d67bcec4d3f388
vinit-j <111049801+vinit-j@users.noreply.github.com>
2023-07-17 14:13:48 +0530

    Add files via upload

---
+++ bubble sort.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int size)
+ {
+     for (int i = 0; i < size - 1; i++)
+        {
+            for (int j = 0; j < size - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1]) {
+                // Swap elements
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int size;
+    cout << "Enter the size of the array: ";
+    cin >> size;
+
+    int arr[size];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Original array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+
+    bubbleSort(arr, size);
+
+    cout << "\nSorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
