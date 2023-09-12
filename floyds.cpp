e6a4d93e87163145f17acceb79635f9943580e46
vinit-j <111049801+vinit-j@users.noreply.github.com>
2023-09-12 15:33:38 +0530

    Update floyds.cpp

--- floyds.cpp
+++ floyds.cpp
@@ -4,49 +4,68 @@ using namespace std;
 
 const int INF = INT_MAX;
 
-void floydWarshall(int graph[][4], int V) {
-    int dist[V][V];
-
-    for (int i = 0; i < V; i++) {
-        for (int j = 0; j < V; j++) {
-            dist[i][j] = graph[i][j];
+class FloydWarshall {
+public:
+    FloydWarshall(int V) : V(V) {
+        dist = new int*[V];
+        for (int i = 0; i < V; i++) {
+            dist[i] = new int[V];
+            for (int j = 0; j < V; j++) {
+                dist[i][j] = graph[i][j];
+            }
         }
     }
 
-    for (int k = 0; k < V; k++) {
+    ~FloydWarshall() {
         for (int i = 0; i < V; i++) {
-            for (int j = 0; j < V; j++) {
-                if (dist[i][k] != INF && dist[k][j] != INF && dist[i][k] + dist[k][j] < dist[i][j]) {
-                    dist[i][j] = dist[i][k] + dist[k][j];
+            delete[] dist[i];
+        }
+        delete[] dist;
+    }
+
+    void findShortestPaths() {
+        for (int k = 0; k < V; k++) {
+            for (int i = 0; i < V; i++) {
+                for (int j = 0; j < V; j++) {
+                    if (dist[i][k] != INF && dist[k][j] != INF && dist[i][k] + dist[k][j] < dist[i][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
                 }
             }
         }
     }
 
-    cout << "Shortest distances between all pairs of vertices:\n";
-    for (int i = 0; i < V; i++) {
-        for (int j = 0; j < V; j++) {
-            if (dist[i][j] == INF) {
-                cout << "INF ";
-            } else {
-                cout << dist[i][j] << " ";
+    void displayShortestPaths() {
+        cout << "Shortest distances between all pairs of vertices:\n";
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][j] == INF) {
+                    cout << "INF ";
+                } else {
+                    cout << dist[i][j] << " ";
+                }
             }
+            cout << endl;
         }
-        cout << endl;
     }
-}
 
-int main() {
-    int V = 4;
+private:
+    int V;
+    int** dist;
     int graph[4][4] = {
         {0, 5, INF, 10},
         {INF, 0, 3, INF},
         {INF, INF, 0, 1},
         {INF, INF, INF, 0}
     };
+};
 
-    floydWarshall(graph, V);
+int main() {
+    int V = 4;
+
+    FloydWarshall floydWarshall(V);
+    floydWarshall.findShortestPaths();
+    floydWarshall.displayShortestPaths();
 
     return 0;
 }
-
