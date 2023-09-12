6d9624137bcb83e8e5df711b6f230986c6466ef6
vinit-j <111049801+vinit-j@users.noreply.github.com>
2023-09-12 15:42:03 +0530

    Update knapsack.cpp

--- knapsack.cpp
+++ knapsack.cpp
@@ -1,44 +1,62 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int knapsack(int weights[], int values[], int n, int capacity) {
-    int dp[n + 1][capacity + 1];
+class Knapsack {
+public:
+    Knapsack(int n, int capacity) : n(n), capacity(capacity) {
+        weights.resize(n);
+        values.resize(n);
+        dp.resize(n + 1, vector<int>(capacity + 1, 0));
+    }
 
-    for (int i = 0; i <= n; i++) {
-        for (int w = 0; w <= capacity; w++) {
-            if (i == 0 || w == 0) {
-                dp[i][w] = 0;
-            } else if (weights[i - 1] <= w) {
-                dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w]);
-            } else {
-                dp[i][w] = dp[i - 1][w];
-            }
+    void inputItems() {
+        cout << "Enter the weights of items:\n";
+        for (int i = 0; i < n; i++) {
+            cin >> weights[i];
+        }
+
+        cout << "Enter the values of items:\n";
+        for (int i = 0; i < n; i++) {
+            cin >> values[i];
         }
     }
 
-    return dp[n][capacity];
-}
+    int knapsack() {
+        for (int i = 0; i <= n; i++) {
+            for (int w = 0; w <= capacity; w++) {
+                if (i == 0 || w == 0) {
+                    dp[i][w] = 0;
+                } else if (weights[i - 1] <= w) {
+                    dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w]);
+                } else {
+                    dp[i][w] = dp[i - 1][w];
+                }
+            }
+        }
+        return dp[n][capacity];
+    }
+
+private:
+    int n;
+    int capacity;
+    vector<int> weights;
+    vector<int> values;
+    vector<vector<int>> dp;
+};
 
 int main() {
     int n, capacity;
     cout << "Enter the number of items: ";
     cin >> n;
 
-    int weights[n], values[n];
-    cout << "Enter the weights of items:\n";
-    for (int i = 0; i < n; i++) {
-        cin >> weights[i];
-    }
-
-    cout << "Enter the values of items:\n";
-    for (int i = 0; i < n; i++) {
-        cin >> values[i];
-    }
-
     cout << "Enter the knapsack capacity: ";
     cin >> capacity;
 
-    int maxProfit = knapsack(weights, values, n, capacity);
+    Knapsack knapsack(n, capacity);
+    knapsack.inputItems();
+    int maxProfit = knapsack.knapsack();
+    
     cout << "Maximum profit: " << maxProfit << endl;
 
     return 0;
