b98fe23a0ea51b3cf05195d68da433932385b00f
vinit-j <111049801+vinit-j@users.noreply.github.com>
2023-09-12 15:35:19 +0530

    Update heap.cpp

--- heap.cpp
+++ heap.cpp
@@ -3,35 +3,79 @@
 #include <cstdlib>
 using namespace std;
 
-void heapify(int arr[], int n, int i) {
-    int largest = i;
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
-
-    if (left < n && arr[left] > arr[largest]) {
-        largest = left;
+class HeapSort {
+public:
+    HeapSort(int n) : N(n) {
+        arr = new int[N];
     }
 
-    if (right < n && arr[right] > arr[largest]) {
-        largest = right;
+    ~HeapSort() {
+        delete[] arr;
     }
 
-    if (largest != i) {
-        swap(arr[i], arr[largest]);
-        heapify(arr, n, largest);
-    }
-}
-
-void heapSort(int arr[], int n) {
-    for (int i = n / 2 - 1; i >= 0; i--) {
-        heapify(arr, n, i);
+    void inputArray() {
+        cout << "Enter " << N << " array elements:\n";
+        for (int i = 0; i < N; i++) {
+            cin >> arr[i];
+        }
     }
 
-    for (int i = n - 1; i > 0; i--) {
-        swap(arr[0], arr[i]);
-        heapify(arr, i, 0);
+    void generateRandomArray() {
+        cout << "Array elements generated randomly.\n";
+        for (int i = 0; i < N; i++) {
+            arr[i] = rand() % 1000; // Adjust the range as needed
+        }
     }
-}
+
+    void sort() {
+        for (int i = N / 2 - 1; i >= 0; i--) {
+            heapify(arr, N, i);
+        }
+
+        for (int i = N - 1; i > 0; i--) {
+            swap(arr[0], arr[i]);
+            heapify(arr, i, 0);
+        }
+    }
+
+    void displaySortedArray() {
+        cout << "Sorted array:\n";
+        for (int i = 0; i < N; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+
+    double measureTimeTaken() {
+        clock_t startTime = clock();
+        sort();
+        clock_t endTime = clock();
+        return double(endTime - startTime) / CLOCKS_PER_SEC;
+    }
+
+private:
+    int* arr;
+    int N;
+
+    void heapify(int arr[], int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        if (largest != i) {
+            swap(arr[i], arr[largest]);
+            heapify(arr, n, largest);
+        }
+    }
+};
 
 int main() {
     srand(time(0));
@@ -40,7 +84,8 @@ int main() {
     cout << "Enter the number of elements: ";
     cin >> N;
 
-    int arr[N];
+    HeapSort heapSort(N);
+
     cout << "Choose an option:\n";
     cout << "1. Manual entry of array elements\n";
     cout << "2. Random number generation for array elements\n";
@@ -48,33 +93,18 @@ int main() {
     cin >> choice;
 
     if (choice == 1) {
-        cout << "Enter " << N << " array elements:\n";
-        for (int i = 0; i < N; i++) {
-            cin >> arr[i];
-        }
+        heapSort.inputArray();
     } else if (choice == 2) {
-        cout << "Array elements generated randomly.\n";
-        for (int i = 0; i < N; i++) {
-            arr[i] = rand() % 1000; // Adjust the range as needed
-        }
+        heapSort.generateRandomArray();
     } else {
         cout << "Invalid choice.\n";
         return 1;
     }
 
-    clock_t startTime = clock();
-    heapSort(arr, N);
-    clock_t endTime = clock();
+    double timeTaken = heapSort.measureTimeTaken();
 
-    cout << "Sorted array:\n";
-    for (int i = 0; i < N; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-
-    double timeTaken = double(endTime - startTime) / CLOCKS_PER_SEC;
+    heapSort.displaySortedArray();
     cout << "Time taken: " << timeTaken << " seconds\n";
 
     return 0;
 }
-
