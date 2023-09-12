29263c810741a96b6e450af9a283eec908b17801
vinit-j <111049801+vinit-j@users.noreply.github.com>
2023-09-12 15:29:39 +0530

    Update bubble sort.cpp

--- bubble sort.cpp
+++ bubble sort.cpp
@@ -1,47 +1,70 @@
 #include <iostream>
 using namespace std;
 
-void bubbleSort(int arr[], int size)
- {
-     for (int i = 0; i < size - 1; i++)
-        {
-            for (int j = 0; j < size - i - 1; j++)
-            {
+class BubbleSort {
+public:
+    BubbleSort(int size) : size(size) {
+        arr = new int[size];
+    }
+
+    ~BubbleSort() {
+        delete[] arr;
+    }
+
+    void input() {
+        cout << "Enter the elements of the array: ";
+        for (int i = 0; i < size; i++) {
+            cin >> arr[i];
+        }
+    }
+
+    void sort() {
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                // Swap elements
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                    // Swap elements
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
             }
         }
     }
-}
 
-int main()
-{
+    void display() {
+        cout << "Sorted array: ";
+        for (int i = 0; i < size; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+
+    int* getArray() {
+        return arr;
+    }
+
+private:
+    int* arr;
+    int size;
+};
+
+int main() {
     int size;
     cout << "Enter the size of the array: ";
     cin >> size;
 
-    int arr[size];
-    cout << "Enter the elements of the array: ";
-    for (int i = 0; i < size; i++) {
-        cin >> arr[i];
-    }
+    BubbleSort sorter(size);
+
+    sorter.input();
 
     cout << "Original array: ";
     for (int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
+        cout << sorter.getArray()[i] << " ";
     }
 
-    bubbleSort(arr, size);
+    sorter.sort();
 
-    cout << "\nSorted array: ";
-    for (int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
-    }
-
-    cout << endl;
+    sorter.display();
 
     return 0;
 }
