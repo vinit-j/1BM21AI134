a0ba2882e5dd6034bbf3a783d831dda2cc1c0b06
vinit-j <111049801+vinit-j@users.noreply.github.com>
2023-09-12 16:07:02 +0530

    Update topo.cpp

--- topo.cpp
+++ topo.cpp
@@ -1,57 +1,63 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+
 using namespace std;
 
-vector<vector<int>> adj;
-vector<int> indegree;
+class Graph {
+public:
+    int n;
+    vector<vector<int>> adj;
+    vector<int> indegree;
 
-void addEdge(int u, int v) {
-    adj[u].push_back(v);
-    indegree[v]++;
-}
+    Graph(int vertices) : n(vertices), adj(vertices), indegree(vertices, 0) {}
 
-vector<int> topologicalSort(int n) {
-    vector<int> result;
-    queue<int> q;
-
-    for (int i = 0; i < n; i++) {
-        if (indegree[i] == 0) {
-            q.push(i);
-        }
+    void addEdge(int u, int v) {
+        adj[u].push_back(v);
+        indegree[v]++;
     }
 
-    while (!q.empty()) {
-        int u = q.front();
-        q.pop();
-        result.push_back(u);
+    vector<int> topologicalSort() {
+        vector<int> result;
+        queue<int> q;
 
-        for (int v : adj[u]) {
-            if (--indegree[v] == 0) {
-                q.push(v);
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
             }
         }
-    }
 
-    return result;
-}
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            result.push_back(u);
+
+            for (int v : adj[u]) {
+                if (--indegree[v] == 0) {
+                    q.push(v);
+                }
+            }
+        }
+
+        return result;
+    }
+};
 
 int main() {
     int n, m;
     cout << "Enter number of nodes and edges: ";
     cin >> n >> m;
 
-    adj.resize(n);
-    indegree.resize(n, 0);
+    Graph graph(n);
 
     cout << "Enter edges (u v):" << endl;
     for (int i = 0; i < m; i++) {
         int u, v;
         cin >> u >> v;
-        addEdge(u, v);
+        graph.addEdge(u, v);
     }
 
-    vector<int> sorted = topologicalSort(n);
+    vector<int> sorted = graph.topologicalSort();
 
     cout << "Topological order:" << endl;
     for (int node : sorted) {
