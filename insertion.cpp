806319a98550b5dbb3ca04fa506a8584ee920e2b
vinit-j <111049801+vinit-j@users.noreply.github.com>
2023-09-12 15:40:42 +0530

    Update insertion.cpp

--- insertion.cpp
+++ insertion.cpp
@@ -4,19 +4,63 @@
 
 using namespace std;
 
-void insertionSort(int arr[], int n) {
-    for (int i = 1; i < n; i++) {
-        int key = arr[i];
-        int j = i - 1;
-
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-
-        arr[j + 1] = key;
+class InsertionSort {
+public:
+    InsertionSort(int n) : N(n) {
+        arr = new int[N];
     }
-}
+
+    ~InsertionSort() {
+        delete[] arr;
+    }
+
+    void inputArray() {
+        cout << "Enter " << N << " array elements:\n";
+        for (int i = 0; i < N; i++) {
+            cin >> arr[i];
+        }
+    }
+
+    void generateRandomArray() {
+        cout << "Array elements generated randomly.\n";
+        for (int i = 0; i < N; i++) {
+            arr[i] = rand() % 1000; // Adjust the range as needed
+        }
+    }
+
+    void sort() {
+        for (int i = 1; i < N; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+
+    void displaySortedArray() {
+        cout << "Sorted array:\n";
+        for (int i = 0; i < N; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+
+    double measureTimeTaken() {
+        clock_t startTime = clock();
+        sort();
+        clock_t endTime = clock();
+        return double(endTime - startTime) / CLOCKS_PER_SEC;
+    }
+
+private:
+    int* arr;
+    int N;
+};
 
 int main() {
     srand(time(0));
@@ -25,7 +69,8 @@ int main() {
     cout << "Enter the number of elements: ";
     cin >> N;
 
-    int arr[N];
+    InsertionSort insertionSort(N);
+
     cout << "Choose an option:\n";
     cout << "1. Manual entry of array elements\n";
     cout << "2. Random number generation for array elements\n";
@@ -33,31 +78,17 @@ int main() {
     cin >> choice;
 
     if (choice == 1) {
-        cout << "Enter " << N << " array elements:\n";
-        for (int i = 0; i < N; i++) {
-            cin >> arr[i];
-        }
+        insertionSort.inputArray();
     } else if (choice == 2) {
-        cout << "Array elements generated randomly.\n";
-        for (int i = 0; i < N; i++) {
-            arr[i] = rand() % 1000; // Adjust the range as needed
-        }
+        insertionSort.generateRandomArray();
     } else {
         cout << "Invalid choice.\n";
         return 1;
     }
 
-    clock_t startTime = clock();
-    insertionSort(arr, N);
-    clock_t endTime = clock();
+    double timeTaken = insertionSort.measureTimeTaken();
 
-    cout << "Sorted array:\n";
-    for (int i = 0; i < N; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-
-    double timeTaken = double(endTime - startTime) / CLOCKS_PER_SEC;
+    insertionSort.displaySortedArray();
     cout << "Time taken: " << timeTaken << " seconds\n";
 
     return 0;
