df05b63171b1640d7c1af50839f5b5fe1dbc1f88
vinit-j <111049801+vinit-j@users.noreply.github.com>
2023-09-12 15:53:04 +0530

    Update lin search.cpp

--- lin search.cpp
+++ lin search.cpp
@@ -1,22 +1,27 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-    int arr[10], i, num, index;
-    cout<<"Enter 10 Numbers: ";
-    for(i=0; i<10; i++)
-        cin>>arr[i];
-    cout<<"\nEnter a Number to Search: ";
-    cin>>num;
-    for(i=0; i<10; i++)
-    {
-        if(arr[i]==num)
-        {
-            index = i;
-            break;
+
+class NumberSearch {
+public:
+    void searchNumber() {
+        int arr[10], i, num, index;
+        cout << "Enter 10 Numbers: ";
+        for (i = 0; i < 10; i++)
+            cin >> arr[i];
+        cout << "\nEnter a Number to Search: ";
+        cin >> num;
+        for (i = 0; i < 10; i++) {
+            if (arr[i] == num) {
+                index = i;
+                break;
+            }
         }
+        cout << "\nFound at Index No." << index << endl;
     }
-    cout<<"\nFound at Index No."<<index;
-    cout<<endl;
+};
+
+int main() {
+    NumberSearch searchObj;
+    searchObj.searchNumber();
     return 0;
 }
