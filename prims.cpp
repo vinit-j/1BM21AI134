5550c530e61d5d0cfcc07e9c75d331b470df764f
vinit-j <111049801+vinit-j@users.noreply.github.com>
2023-09-12 15:59:16 +0530

    Update prims.cpp

--- prims.cpp
+++ prims.cpp
@@ -1,82 +1,73 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <climits>
 
+using namespace std;
 
 #define V 5
 
+class MSTPrim {
+public:
+    int minKey(int key[], bool mstSet[]) {
+        int min = INT_MAX, min_index;
 
-int minKey(int key[], bool mstSet[])
-{
+        for (int v = 0; v < V; v++) {
+            if (mstSet[v] == false && key[v] < min) {
+                min = key[v];
+                min_index = v;
+            }
+        }
 
-int min = INT_MAX, min_index;
+        return min_index;
+    }
 
-for (int v = 0; v < V; v++)
-if (mstSet[v] == false && key[v] < min)
-min = key[v], min_index = v;
+    void printMST(int parent[], int graph[V][V]) {
+        cout << "Edge \tWeight\n";
+        for (int i = 1; i < V; i++) {
+            cout << parent[i] << " - " << i << " \t" << graph[i][parent[i]] << " \n";
+        }
+    }
 
-return min_index;
-}
+    void primMST(int graph[V][V]) {
+        int parent[V];
+        int key[V];
+        bool mstSet[V];
 
+        for (int i = 0; i < V; i++) {
+            key[i] = INT_MAX;
+            mstSet[i] = false;
+        }
 
-void printMST(int parent[], int graph[V][V])
-{
-cout << "Edge \tWeight\n";
-for (int i = 1; i < V; i++)
-cout << parent[i] << " - " << i << " \t"
-<< graph[i][parent[i]] << " \n";
-}
+        key[0] = 0;
+        parent[0] = -1;
 
+        for (int count = 0; count < V - 1; count++) {
+            int u = minKey(key, mstSet);
+            mstSet[u] = true;
 
-void primMST(int graph[V][V])
-{
+            for (int v = 0; v < V; v++) {
+                if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v]) {
+                    parent[v] = u;
+                    key[v] = graph[u][v];
+                }
+            }
+        }
 
-int parent[V];
+        printMST(parent, graph);
+    }
+};
 
+int main() {
+    MSTPrim mst;
 
-int key[V];
+    int graph[V][V] = {
+        {0, 2, 0, 6, 0},
+        {2, 0, 3, 8, 5},
+        {0, 3, 0, 0, 7},
+        {6, 8, 0, 0, 9},
+        {0, 5, 7, 9, 0}
+    };
 
-bool mstSet[V];
+    mst.primMST(graph);
 
-
-for (int i = 0; i < V; i++)
-key[i] = INT_MAX, mstSet[i] = false;
-
-
-key[0] = 0;
-
-
-parent[0] = -1;
-
-for (int count = 0; count < V - 1; count++) {
-
-
-int u = minKey(key, mstSet);
-
-
-mstSet[u] = true;
-
-for (int v = 0; v < V; v++)
-
-
-if (graph[u][v] && mstSet[v] == false
-&& graph[u][v] < key[v])
-parent[v] = u, key[v] = graph[u][v];
-}
-
-
-printMST(parent, graph);
-}
-
-
-int main()
-{
-int graph[V][V] = { { 0, 2, 0, 6, 0 },
-{ 2, 0, 3, 8, 5 },
-{ 0, 3, 0, 0, 7 },
-{ 6, 8, 0, 0, 9 },
-{ 0, 5, 7, 9, 0 } };
-
-primMST(graph);
-
-return 0;
+    return 0;
 }
