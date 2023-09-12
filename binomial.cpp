73f46aa461b5327403d061c865c867bb6957a9c4
vinit-j <111049801+vinit-j@users.noreply.github.com>
2023-09-12 15:26:50 +0530

    Update binomial.cpp

--- binomial.cpp
+++ binomial.cpp
@@ -1,28 +1,44 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int binomialCoefficient(int n, int k) {
-    int dp[n + 1][k + 1];
-
-    for (int i = 0; i <= n; i++) {
-        for (int j = 0; j <= min(i, k); j++) {
-            if (j == 0 || j == i) {
-                dp[i][j] = 1;
-            } else {
-                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
-            }
-        }
+class BinomialCoefficientCalculator {
+public:
+    BinomialCoefficientCalculator(int n, int k) : n(n), k(k) {
+        dp.assign(n + 1, vector<int>(k + 1, -1));
     }
 
-    return dp[n][k];
-}
+    int calculate() {
+        return calculateBinomialCoefficient(n, k);
+    }
+
+private:
+    vector<vector<int>> dp;
+    int n, k;
+
+    int calculateBinomialCoefficient(int n, int k) {
+        if (k == 0 || k == n) {
+            return 1;
+        }
+
+        if (dp[n][k] != -1) {
+            return dp[n][k];
+        }
+
+        dp[n][k] = calculateBinomialCoefficient(n - 1, k - 1) + calculateBinomialCoefficient(n - 1, k);
+        return dp[n][k];
+    }
+};
 
 int main() {
     int n, k;
     cout << "Enter values of n and k: ";
     cin >> n >> k;
 
-    cout << "Binomial Coefficient C(" << n << ", " << k << ") = " << binomialCoefficient(n, k) << endl;
+    BinomialCoefficientCalculator calculator(n, k);
+    int result = calculator.calculate();
+
+    cout << "Binomial Coefficient C(" << n << ", " << k << ") = " << result << endl;
 
     return 0;
 }
